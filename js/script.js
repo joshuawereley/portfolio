0857ad1941f2e3cd7427e2bbafd53138032f73ea
Joshua Wereley <66910539+joshuawereley@users.noreply.github.com>
2023-07-15 10:25:43 +0200

    Update script.js
    Updated the websites functionality

--- js/script.js
+++ js/script.js
@@ -6,31 +6,6 @@ menuIcon.onclick = () => {
    navbar.classList.toggle('active');
 } 
 
-/*
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-   sections.forEach(sec => {
-      let top = window.scrollY;
-      let offset = sec.offsetTop - 100;
-      let height = sec.offsetHeight;
-      let id = sec.getAttribute('id');
-      if (top >= offset && top < offset + height) {
-         navLinks.forEach(links => {
-            links.classList.remove('actuve');
-            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-         });
-      }
-   });
-
-   let header = document.querySelector('header');
-   header.classList.toggle('sticky', window.scrollY > 100);
-
-   menuIcon.classList.remove('bx-x');
-   navbar.classList.remove('active');
-}
-*/
 
 const navLinks = document.querySelectorAll('.navbar a');
 navLinks.forEach((link) => {
@@ -75,4 +50,3 @@ window.addEventListener('scroll', () => {
    menuIcon.classList.remove('bx-x');
    navbar.classList.remove('active');
 });
-
