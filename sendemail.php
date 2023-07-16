0a8cb59710dbfe1a0515e25eaebe144c8370eb59
Joshua Wereley <66910539+joshuawereley@users.noreply.github.com>
2023-07-16 13:41:19 +0200

    Create sendemail.php
    Created sendmail.php so that it's possible to send emails to me

---
+++ sendemail.php
@@ -0,0 +1,15 @@
+<?php
+   $name = trim(strip_tags($_POST['name']));
+   $email = trim(strip_tags($_POST['email']));
+   $phone = trim(strip_tags($_POST['phone']));
+   $subject = trim(strip_tags($_POST['subject']))
+   $message = htmlentities($_POST['message']);
+   $to = 'u21506915@tuks.co.za';
+   $body = <<<HTML
+   $message
+   $phone
+   HTML;
+   $headers = "From: $email\r\n";
+   $headers .= "Content-type: text/html\r\n"
+   mail($to, $subject, $body, $headers);
+?>
