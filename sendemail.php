3723d72070da803f5bdcec7ed1bc85303da2f15e
Joshua Wereley <66910539+joshuawereley@users.noreply.github.com>
2023-07-17 09:20:11 +0200

    Update sendemail.php
    Added a semi-colon

--- sendemail.php
+++ sendemail.php
@@ -2,7 +2,7 @@
    $name = trim(strip_tags($_POST['name']));
    $email = trim(strip_tags($_POST['email']));
    $phone = trim(strip_tags($_POST['phone']));
-   $subject = trim(strip_tags($_POST['subject']))
+   $subject = trim(strip_tags($_POST['subject']));
    $message = htmlentities($_POST['message']);
    $to = 'u21506915@tuks.co.za';
    $body = <<<HTML
